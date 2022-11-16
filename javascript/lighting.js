59f85529d6c0e1f35c6820337d672e7bec1433bd
shrubfromtomorrow <orionjandrews@gmail.com>
2022-11-15 20:12:51 -0800

    Finished carousel JS and css as well as added in my own images :(. Need to tune the buttons

--- javascript/lighting.js
+++ javascript/lighting.js
@@ -59,4 +59,18 @@ seniorLinkList.addEventListener("click", () => {
   }
 });
 
+const buttons = document.querySelectorAll("[data-carousel-button]");
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const offest = button.dataset.carouselButton === "next" ? 1 : -1;
+    const slides = button.closest("[data-carousel]").querySelector("[data-slides]");
+    const activeSlide = slides.querySelector("[data-active]");
+    let newIndex = [...slides.children].indexOf(activeSlide) + offest;
+    if (newIndex < 0) newIndex = slides.children.length - 1;
+    if (newIndex >= slides.children.length) newIndex = 0;
+    slides.children[newIndex].dataset.active = true;
+    delete activeSlide.dataset.active;
+  })
+})
+
 
